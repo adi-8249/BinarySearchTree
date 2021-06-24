ff742faa0084632db1037d116c9ec9e7a89f7985
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-24 23:04:30 +0530

    [Anjaneya]Added:Inserting the given elements

--- BinarySearchTreeProgram/BinarySearchTreeProgram/Program.cs
+++ BinarySearchTreeProgram/BinarySearchTreeProgram/Program.cs
@@ -7,6 +7,11 @@ namespace BinarySearchTreeProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search progarm");
+            BinarySearch binary = new BinarySearch();
+            binary.Insert(56);
+            binary.Insert(30);
+            binary.Insert(70);
+            binary.Display();
         }
     }
 }
---
+++ BinarySearchTreeProgram/BinarySearchTreeProgram/TreeNode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeProgram
+{
+    class TreeNode
+    {
+
+        public int data;
+        public TreeNode leftNode;
+        public TreeNode rightNode;
+        public TreeNode(int data)//initialising the data using constructor
+        {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+        public void Insert(int data)//inserting data through Insert method
+        {
+            int rootData = this.data;
+            if (data >= rootData)
+            {
+                if (rightNode == null)
+                {
+                    rightNode = new TreeNode(data);
+                }
+                else
+                {
+                    rightNode.Insert(data);
+                }
+            }
+            else
+            {
+                if (leftNode == null)
+                {
+                    leftNode = new TreeNode(data);
+                }
+                else
+                {
+                    leftNode.Insert(data);
+                }
+            }
+        }
+        //Displaying as leftNode -> rootNode -> rightNode
+        public void InOrderTraversal()
+        {
+            if (leftNode != null)
+            {
+                leftNode.InOrderTraversal();
+            }
+            Console.Write(data + " ");
+            if (rightNode != null)
+            {
+                rightNode.InOrderTraversal();
+            }
+        }
+
+    }
+}
